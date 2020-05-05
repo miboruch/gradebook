0235093988c5a7e53e22e9f0e770e8d237e46f95
Michał Boruch <miboruch@gmail.com>
2020-05-05 12:45:13 +0200

    Added input component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import Input from './components/atoms/Input/Input';
 
 function App() {
   return (
     <Layout>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Input />
     </Layout>
   );
 }
--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -2,12 +2,59 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Input = () => {
-  return (
-    <div>
+const InputWrapper = styled.div`
+  width: 300px;
+  height: 40px;
+  background-color: ${({ theme }) => theme.color.searchBar};
+  border: ${({ isDarkTheme }) => (isDarkTheme ? '1px solid #555' : 'none')};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+  flex-direction: row;
+  border-radius: 20px;
+  transition: all 0.4s ease;
+  ${({ theme }) => theme.mq.tablet} {
+    width: 550px;
+  }
+  ${({ theme }) => theme.mq.standard} {
+    width: 450px;
+    margin-top: 0;
+  }
+`;
 
-    </div>
+const StyledInput = styled.input`
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  border: none;
+  padding-left: 2rem;
+  font-size: 14px;
+  color: ${({ theme }) => theme.color.fontColor};
+  font-family: 'Gilroy';
+  &::placeholder {
+    font-size: 15px;
+    position: absolute;
+    color: #c9c9c9;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Input = ({ type, handleChange, handleBlur }) => {
+  return (
+    <InputWrapper>
+      <StyledInput type={type} onChange={handleChange} onBlur={handleBlur} />
+    </InputWrapper>
   );
+};
+
+Input.propTypes = {
+  type: PropTypes.string,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func
 };
 
 export default Input;
