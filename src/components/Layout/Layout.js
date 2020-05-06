d039d3920f11c369ad1b57d9d2e2fb7e2f2f33d9
Michał Boruch <miboruch@gmail.com>
2020-05-06 13:22:42 +0200

    Added hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import { theme } from '../../style/theme';
 import GlobalStyle from '../../style/GlobalStyle';
 import SEO from '../SEO';
 
+const StyledWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
 const Layout = ({ children }) => {
   return (
     <>
       <SEO />
       <GlobalStyle />
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <StyledWrapper>{children}</StyledWrapper>
+      </ThemeProvider>
     </>
   );
 };
