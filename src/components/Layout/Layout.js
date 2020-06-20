606725f3918834a0a770291648d8cb912f30cf40
Michał Boruch <miboruch@gmail.com>
2020-06-20 15:07:33 +0200

    Atoms styles separated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import { theme } from '../../style/theme';
@@ -7,36 +6,21 @@ import GlobalStyle from '../../style/GlobalStyle';
 import SEO from '../SEO';
 import Hamburger from '../atoms/Hamburger/Hamburger';
 import Slider from '../molecules/Slider/Slider';
+import { StyledWrapper, ContentWrapper } from './Layout.styles';
 
-const StyledWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  overflow: hidden;
-`;
-
-const ContentWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  position: relative;
-`;
-
-const Layout = ({ children }) => {
-  return (
-    <>
-      <SEO />
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <StyledWrapper>
-          <Hamburger />
-          <Slider />
-          <ContentWrapper>{children}</ContentWrapper>
-        </StyledWrapper>
-      </ThemeProvider>
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <SEO />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <StyledWrapper>
+        <Hamburger />
+        <Slider />
+        <ContentWrapper>{children}</ContentWrapper>
+      </StyledWrapper>
+    </ThemeProvider>
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
---
+++ src/components/Layout/Layout.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+export { StyledWrapper, ContentWrapper };
